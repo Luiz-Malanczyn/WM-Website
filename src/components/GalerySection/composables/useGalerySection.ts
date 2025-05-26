0c665facaa4b80bd4e528334a5f385eaca4e6f1e
Luiz Eduardo Malanczyn de Oliveira <oliveiraluiz.com@hotmail.com>
2025-05-26 11:13:45 -0300

    Changed some images related to trucks

--- src/components/GalerySection/composables/useGalerySection.ts
+++ src/components/GalerySection/composables/useGalerySection.ts
@@ -5,6 +5,7 @@ import imagemGaleria3 from '../../../assets/imagem-galeria-3.png'
 import imagemGaleria4 from '../../../assets/imagem-galeria-4.png'
 import imagemGaleria5 from '../../../assets/imagem-galeria-5.png'
 import imagemGaleria6 from '../../../assets/imagem-galeria-6.png'
+import imagemGaleria7 from '../../../assets/imagem-galeria-7.png'
 import videoGaleria1 from '../../../assets/video-galeria-1.mp4'
 import videoGaleria2 from '../../../assets/video-galeria-2.mp4'
 import videoGaleria3 from '../../../assets/video-galeria-3.mp4'
@@ -26,12 +27,13 @@ export const galleryImages: GalleryItem[] = [
   { id: 4, src: imagemGaleria4, alt: 'Galeria 4', type: 'image' },
   { id: 5, src: imagemGaleria5, alt: 'Galeria 5', type: 'image' },
   { id: 6, src: imagemGaleria6, alt: 'Galeria 6', type: 'image' },
-  { id: 7, src: videoGaleria1, alt: 'Video 1', type: 'video' },
-  { id: 8, src: videoGaleria2, alt: 'Video 2', type: 'video' },
-  { id: 9, src: videoGaleria3, alt: 'Video 3', type: 'video' },
-  { id: 10, src: videoGaleria4, alt: 'Video 4', type: 'video' },
-  { id: 11, src: videoGaleria5, alt: 'Video 5', type: 'video' },
-  { id: 12, src: videoGaleria6, alt: 'Video 6', type: 'video' },
+  { id: 7, src: imagemGaleria7, alt: 'Galeria 7', type: 'image' },
+  { id: 8, src: videoGaleria1, alt: 'Video 1', type: 'video' },
+  { id: 9, src: videoGaleria2, alt: 'Video 2', type: 'video' },
+  { id: 10, src: videoGaleria3, alt: 'Video 3', type: 'video' },
+  { id: 11, src: videoGaleria4, alt: 'Video 4', type: 'video' },
+  { id: 12, src: videoGaleria5, alt: 'Video 5', type: 'video' },
+  { id: 13, src: videoGaleria6, alt: 'Video 6', type: 'video' },
 ]
 
 export const useGalerySection = () => {
