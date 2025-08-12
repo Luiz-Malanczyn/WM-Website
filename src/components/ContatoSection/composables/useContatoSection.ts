95bdb00ee261cf8953b8e17361e7bd55acaff725
Luiz Eduardo Malanczyn de Oliveira <oliveiraluiz.com@hotmail.com>
2025-08-12 18:40:25 -0300

    Adding 9 to celphone numbers

--- src/components/ContatoSection/composables/useContatoSection.ts
+++ src/components/ContatoSection/composables/useContatoSection.ts
@@ -6,13 +6,13 @@ export const contactInfo = {
   comercial: {
     titulo: 'Contato Comercial',
     telefones: ['(41) 3069-0003', '(41) 3069-0005'],
-    celulares: ['(41) 9971-0724'],
+    celulares: ['(41) 99971-0724'],
     email: 'wagner@wmtermoplasticos.com.br'
   },
   financeiro: {
     titulo: 'Contato ADM/Financeiro',
     telefones: ['(41) 3069-0003', '(41) 3069-0005'],
-    celulares: ['(41) 9971-1278'],
+    celulares: ['(41) 99971-1278'],
     email: 'fabiana@wmtermoplasticos.com.br'
   },
   recursosHumanos: {
