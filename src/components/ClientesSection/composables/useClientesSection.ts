4eff0420954f91ccf52c2ce7da7540b63932ee37
Luiz Eduardo Malanczyn de Oliveira <oliveiraluiz.com@hotmail.com>
2026-01-26 09:48:02 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/ClientesSection/composables/useClientesSection.ts
+++ src/components/ClientesSection/composables/useClientesSection.ts
@@ -1,11 +1,9 @@
 import imagemElectrolux from '../../../assets/imagem-electrolux.png'
 import imagemGelopar from '../../../assets/imagem-gelopar.png'
-import imagemWap from '../../../assets/imagem-wap.png'
 import imagemKabel from '../../../assets/imagem-kabel.png'
 
 export const clients = [
   { name: 'Electrolux', image: imagemElectrolux },
   { name: 'Gelopar', image: imagemGelopar },
-  { name: 'WAP', image: imagemWap },
   { name: 'Kabel', image: imagemKabel },
 ]
